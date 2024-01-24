c5f2023471f1991eb8b470269eb128b476b7cac8
Vu Viet Hong Quan <vuviethongquan@gmail.com>
2024-01-24 21:16:49 +0700

    '

--- MyPaint/OOP_Form3/FactoryMethod/RandomFactory.cs
+++ MyPaint/OOP_Form3/FactoryMethod/RandomFactory.cs
@@ -19,16 +19,12 @@ namespace MyPaint.OOP_Form3.FactoryMethod
         {
             Random random = new Random();
 
-            // Sinh ra một số ngẫu nhiên từ 0 đến 1 để chọn loại hình
             int shapeType = random.Next(6);
 
-            // Sinh ra một số ngẫu nhiên từ 0 đến 100 để chọn màu sắc
             int colorIndex = random.Next(100);
 
-            // Tạo một mảng các màu sắc có sẵn
             Color[] colors = { Color.Red, Color.Green, Color.Blue, Color.Yellow, Color.Pink, Color.Purple, Color.Orange, Color.Brown, Color.Black, Color.White };
 
-            // Chọn một màu sắc ngẫu nhiên từ mảng
             Color color = colors[colorIndex % colors.Length];
 
             if (shapeType == 0)
